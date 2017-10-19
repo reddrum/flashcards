af44a1746a70d262c1ef280807043d2d4c0494f6
“reddrum” <anton.kranz@me.com>
2017-10-19 19:18:32 +0300

    Add CRUD&validations

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'pg', '~> 0.21.0'
 gem 'rb-readline', '~> 0.5.5'
+gem 'simple_form', '~> 3.5'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,8 @@
 class Card < ApplicationRecord
+  validates :original_text, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :translated_text, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[а-яА-Я]+\z/ }
+  
+  before_create do
+    self.review_date = DateTime.now.midnight + 3.days
+  end
 end
