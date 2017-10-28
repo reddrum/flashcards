b73c75f74c610d5b002c0252ec39effc2e634eb7
“reddrum” <anton.kranz@me.com>
2017-10-28 03:00:24 +0300

    Setup FactoryBot&add tests

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,20 +2,22 @@ require "rails_helper"
 
 RSpec.describe Card, :type => :model do
   
-  context "validate" do 
-    let(:card) { create(:card, original_text: "home", translated_text: "home") }
+  context "is not valid" do 
+    let(:card) { build(:card, original_text: "home", translated_text: "home") }
+    it { expect(card).to_not be_valid }
+  end
 
-    it "invalid_card" do       
-      expect(card).to_not be_valid
-    end
+  context "is valid" do
+    let(:card) { create(:card) }
+    it { expect(card).to be_valid }    
   end
 
   context "checks" do
     let(:card) { create(:card) }
 
-    it "null_review_date" do      
-      card.review_date = nil
-      expect(card).to_not be_valid
+    it "review_date" do      
+      expect(card.review_date.to_s).to eq (Date.today + 3.days).to_s
     end
   end
 end
+
